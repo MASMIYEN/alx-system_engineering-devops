e38bcaf3b787d06c2b2ce54dfbbea0d12e78c134
MASMIYEN <masmiyen@gmail.com>
2023-12-23 00:35:49 +0100

    this should fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,25 @@
-# install flask version 2.1.0 with pip3
+# install flask and required packages
+
+#Install python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+  }
+
+#install pip3
+package { 'python3-pip':
+  ensure => present,
+  }
+
+#install flask
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip']
 }
+
+#install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+  }
