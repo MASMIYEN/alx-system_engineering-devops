83812bb16a6f6055ca78bcd04c1d6a01820486ea
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-01-29 17:56:20 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
 """ gather data from API """
+import sys
+import requests
+
+def main():
+    """ main function """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/users/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done =  f'todos?userId={id}&completed=true'
+    not_done = f'todos?userId={id}&completed=false'
+    user_data = requests.get(url + users).json()
+    name = user_data[0].get('name')
+    todos_data = requests.get(url + todos).json()
+    done_data = requests.get(url + done).json()
+    done_ = len(done_data)
+    total_done = len(todos_data)
+    print(f'Employee {name} is done with tasks({done_}/{total_done}):')
+    for task in done_data:
+        print(f'\t {task.get("title")}')
+
+
+if __name__ == "__main__":
+    main()
