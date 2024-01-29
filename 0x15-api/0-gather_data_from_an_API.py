4eaa220b0ef9b5d96168ed38eac44896eab6dd34
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-01-29 18:00:10 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,14 @@
 import sys
 import requests
 
+
 def main():
     """ main function """
     id = sys.argv[1]
     url = f'https://jsonplaceholder.typicode.com/users/'
     users = f'users?id={id}'
     todos = f'todos?userId={id}'
-    done =  f'todos?userId={id}&completed=true'
+    done = f'todos?userId={id}&completed=true'
     not_done = f'todos?userId={id}&completed=false'
     user_data = requests.get(url + users).json()
     name = user_data[0].get('name')
@@ -24,3 +25,55 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+
+Employee *NAME* is done with tasks(*DONE*/*TOTAL*):
+     *TITLE*
+     *TITLE*
+     *TITLE*
+
+
+https://jsonplaceholder.typicode.com/users?id=1
+{
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+https://jsonplaceholder.typicode.com/todos?userId=5
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  ]
+
+https://jsonplaceholder.typicode.com/todos?userId=5&completed=true
+https://jsonplaceholder.typicode.com/todos?userId=5&completed=false
+  """
