4c6f4660467d9e8468d1a539097a94bd8edf1cc6
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-02-13 14:50:11 +0100

    less docstring

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
-#!/user/bin/python3
-"""queries the Reddit API and
-returns the number of subscribers"""
+#!/usr/bin/python3
+"""
+this doc for module
+"""
 import requests
 
-headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    """method"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        return data["data"]["subscribers"]
     else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""queries the Reddit API and
-prints the titles of the
-first 10 hot posts listed
-for a given subreddit."""
+"""
+this doc for module
+"""
 import requests
 
-headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def top_ten(subreddit):
-    """method."""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        for post in data['data']['children']:
-            print(post['data']['title'])
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
     else:
-        print('None')
+        print("None")
