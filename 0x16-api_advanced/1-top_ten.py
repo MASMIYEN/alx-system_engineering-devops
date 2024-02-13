03cd798e17860499d99319733d2843d1afc0a8d3
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-02-13 15:35:58 +0100

    top10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""A module containing functions for working with the Reddit API.
-"""
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
-BASE_URL = "https://www.reddit.com"
-"""Reddit's base API URL.
-"""
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
 def top_ten(subreddit):
-    """Retrieves the title of the top ten posts from a given subreddit."""
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
     api_headers = {
-        "Accept": "application/json",
-        "User-Agent": " ".join(
-            [
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-                "AppleWebKit/537.36 (KHTML, like Gecko)",
-                "Chrome/97.0.4692.71",
-                "Safari/537.36",
-                "Edg/97.0.1072.62",
-            ]
-        ),
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
-    sort = "top"
+    sort = 'top'
     limit = 10
     res = requests.get(
-        "{}/r/{}/.json?sort={}&limit={}".format(
-            BASE_URL, subreddit, sort, limit),
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
         headers=api_headers,
-        allow_redirects=False,
+        allow_redirects=False
     )
     if res.status_code == 200:
-        for post in res.json()["data"]["children"][:10]:
-            print(post["data"]["title"])
+        for post in res.json()['data']['children'][0:10]:
+            print(post['data']['title'])
     else:
         print(None)
