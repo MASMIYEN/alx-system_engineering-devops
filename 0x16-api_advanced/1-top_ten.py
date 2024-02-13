a3b7441bc77f918d2622e6d1c047cbfc8c71122e
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-02-13 14:48:58 +0100

    less docstring

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit."""
+"""queries the Reddit API and
+prints the titles of the
+first 10 hot posts listed
+for a given subreddit."""
 import requests
 
 headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
