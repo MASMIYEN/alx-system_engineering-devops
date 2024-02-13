bf489d1931496fd6107d8a6b03f991f66b844530
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-02-13 14:59:17 +0100

    top10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,22 @@ this doc for module
 """
 import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+# headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+headers = {
+    "User-Agent": "lMyCustomUserAgent/1.0"
+}
+params = {
+    "limit": 10
+}
 
 
 def top_ten(subreddit):
-    """method doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
