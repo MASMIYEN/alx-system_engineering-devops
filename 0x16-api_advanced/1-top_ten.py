d9bb483bd998dad3325bd0bab2cea6f5f301c0ea
MASMIYEN <masmiyen@gmail.com>
2024-02-13 14:42:09 +0100

    top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit."""
+import requests
+
+headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
+
+
+def top_ten(subreddit):
+    """method."""
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print('None')
