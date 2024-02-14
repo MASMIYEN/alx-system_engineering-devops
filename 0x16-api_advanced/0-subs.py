2a40ce21e86c739c883bf17605fe20b3877235c5
MASMIYEN <masmiyen@gmail.com>
2024-02-14 18:21:54 +0100

    fix susbs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,47 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+""" querry reddit api for subreddit info"""
+import requests
+import requests.auth
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """querry reddit api"""
+    sub = subreddit
+    subreddit = "/r/{}/about".format(sub)
+    temp = "HolbertonPass845"
 
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
+    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
+    client_id = "hy4KvoK0W2iDvw"
+    usr_name = "jgadelugo"
+
+    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password", "username": usr_name, "password": temp}
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+    response = requests.post(
+        "https://www.reddit.com/api/v1/access_token",
+        auth=client_auth,
+        data=post_data,
+        headers=headers,
     )
-    if sub_info.status_code >= 300:
-        return 0
+    auth_json = response.json()
 
-    return sub_info.json().get("data").get("subscribers")
+    token_type = auth_json["token_type"]
+    access_token = auth_json["access_token"]
+
+    headers = {
+        "Authorization": "{} {}".format(token_type, access_token),
+        "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name),
+    }
+
+    query = "https://oauth.reddit.com{}".format(subreddit)
+    res = requests.get(query, headers=headers)
+
+    status = res.status_code
+
+    if status != 200:
+        num_of_subs = 0
+    else:
+        info = res.json()
+        num_of_subs = info["data"]["subscribers"]
+
+    return int(num_of_subs)
