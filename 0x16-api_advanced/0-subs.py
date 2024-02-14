7426c999a8a1d7694444ae394a035f70bf1997c3
MASMIYEN <masmiyen@gmail.com>
2024-02-14 18:20:33 +0100

    fix susbs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns the number of subscribers"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers (not
-    active users, total subscribers) for a given subreddit.
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    If not a valid subreddit, return 0.
-    Invalid subreddits may return a redirect to search results. Ensure that
-    you are not following redirects.
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False,
+    )
+    if sub_info.status_code >= 300:
+        return 0
 
-    Args:
-        subreddit (str): subreddit
-
-    Returns:
-        int: number of subscribers
-    """
-    base_url = "https://www.reddit.com/r/"
-
-    url = "{}{}/about.json".format(base_url, subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)"
-    }
-    results = requests.get(url, headers=headers, allow_redirects=False)
-    if results.status_code == 200:
-        return results.json()["data"]["subscribers"]
-    return 0
+    return sub_info.json().get("data").get("subscribers")
